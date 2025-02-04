92f7c5cafd6abe92c6a055d06ddea047363a3eb4
Alfredo <alfredojcpbarbosa@gmail.com>
2025-02-04 12:05:05 -0300

    Create todo backend and a simple interface

--- local.js
+++ local.js
@@ -1,14 +1,70 @@
 
-function getTodo() {
-    const todo = localStorage.getItem("todo");
-    if (todo == null) 
-        return new Array();
-    return JSON.parse(todo);
+function getCurrentDate() {
+    const date = new Date(); 
+    const dateStr = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`; 
+    return dateStr;
 }
 
-function addTodo(newTodo) {
-    const todo = getTodo(); 
-    todo.push(newTodo);
-    localStorage.setItem("todo", JSON.stringify(todo));
+function getId() {
+    const idLocalStorage = localStorage.getItem("id");
+    const id = (idLocalStorage === null) ? 0 : parseInt(idLocalStorage);
+    localStorage.setItem("id", id + 1);
+    return id; 
+}
+
+function getTodoData() {
+    const todoData = localStorage.getItem("todo");
+    if (todoData === null) 
+        return new Array();
+    return JSON.parse(todoData);
+}
+
+function addTodo(title) {
+    const todoData = getTodoData(); 
+    const id = getId();
+    const newTodo = {
+        id: id,
+        title: title,
+        statusHistory: []
+    }
+    todoData.push(newTodo);
+    localStorage.setItem("todo", JSON.stringify(todoData));
+}
+
+function toggleStatus(id) {
+    const todoData = getTodoData();
+    const dateStr = getCurrentDate();
+    let todo;
+
+    for (let i=0; i < todoData.length; i++) {
+        todo = todoData.at(i);
+        if (todo.id === id) {
+            if (getStatus(todo))
+                todo.statusHistory.pop();
+            else
+                todo.statusHistory.push(dateStr);
+        }
+    }
+
+    localStorage.setItem("todo", JSON.stringify(todoData));
+}
+
+function getStatus(todo) {
+    return todo.statusHistory.at(-1) === getCurrentDate();
+}
+
+function removeTodo(id) {
+    const todoData = getTodoData();
+    let todo;
+    
+    for (let i=0; i < todoData.length; i++) {
+        todo = todoData.at(i);
+        if (todo.id === id) {
+            todoData.splice(i, 1);
+            break;
+        }
+    }
+
+    localStorage.setItem("todo", JSON.stringify(todoData));
 }
 
--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 
-function updateTodoDisplay() {
-    const todo = getTodo();
-    const todoDisplay = document.getElementById("todo-display");
-
-    todoDisplay.innerHTML = "";
-
-    todo.forEach((element) => {
-        let item = document.createElement("li");
-        item.textContent = element;
-        todoDisplay.appendChild(item);
-    });
-}
-
 function newTodoButtonClick() {
     const titleInput = document.getElementById("title-input");
     addTodo(titleInput.value);
     updateTodoDisplay();
+}
+
+function trashIconClick(e) {
+    removeTodo(e.currentTarget.todoId); 
+    updateTodoDisplay();
+}
+
+function checkIconClick(e) {
+    toggleStatus(e.currentTarget.todoId);
+    updateTodoDisplay();
+}
+
+function updateTodoDisplay() {
+    const todoData = getTodoData();
+    const todoDisplay = document.getElementById("todo-display");
+    todoDisplay.innerHTML = "";
+
+    todoData.forEach((element) => {
+        let item = document.createElement("li");
+        let checkIconClass = getStatus(element) ? "fa-square-check" : "fa-square"; 
+        let textClass = getStatus(element) ? "checked" : "unchecked";
+
+        item.innerHTML = `
+            <div>
+                <span id="${element.id}-check" class="fa-regular ${checkIconClass}"></span>
+                <span id="${element.id}-todo" class="${textClass}">${element.title}</span>
+                <span id="${element.id}-delete" class="fa fa-trash"></span>
+            </div>
+        `
+        todoDisplay.appendChild(item);
+
+        const checkIcon = document.getElementById(`${element.id}-check`);
+        checkIcon.todoId = element.id;
+        checkIcon.addEventListener("click", checkIconClick);
+        
+        const trashIcon = document.getElementById(`${element.id}-delete`);
+        trashIcon.todoId = element.id;
+        trashIcon.addEventListener("click", trashIconClick);
+    });
+}
+
+function updateCalendar() {
+    const calendar = document.getElementById("calendar-days");
+}
+
+// Compatibilty
+let legacy = true;
+if (localStorage.getItem("legacy-flag") === null || localStorage.getItem("legacy-flag") === legacy) {
+    localStorage.clear();
+    localStorage.setItem("legacy-flag", !legacy);
 }
 
 const newTodoButton = document.getElementById("new-todo-button");
